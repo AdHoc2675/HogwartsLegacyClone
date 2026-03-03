411baa6ab0244ad57dc157a5c62986f03a41ab65
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2026-03-03 17:59:56 +0900

    base GAS file added

---
+++ Source/HogwartsLegacyClone/Public/GAS/Attributes/HOGAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "HOGAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOGWARTSLEGACYCLONE_API UHOGAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
