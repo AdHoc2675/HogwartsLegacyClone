c6087c6ef3a69c6a8b035b69a0d4d2fc8b49d9c2
[Doheechoi] <rndtn47@gmail.com>
2026-03-04 14:09:33 +0900

    feat: DataAsset 기반 EnhancedInput PlayerController 및 플레이어 베이스 이동 구현
    - UDA_InputConfig(DataAsset) 기반 입력 설정 구조 도입
    - AHOG_PlayerController에서 IMC 추가 및 EnhancedInput 바인딩 구현
    - Tag 기반 Ability 입력 라우팅 구조 추가 (Pressed / Released)
    - APlayerCharacterBase에 카메라(SpringArm/Camera), 이동/시점/점프 입력 처리 구현
    - Capsule 크기 및 Mesh 기본 위치/회전 세팅 추가
    - BP_Playerbase 생성 및 GameMode 기본 Pawn 연결
    - 플레이어 이동/시점 입력 정상 동작 확인
    
    close #4

---
+++ Source/HogwartsLegacyClone/Public/Data/DA_InputConfig.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "DA_InputConfig.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+/**
+ * Input 설정 DataAsset
+ * - PlayerController가 읽어서 EnhancedInput 바인딩을 구성한다.
+ */
+
+UCLASS()
+class HOGWARTSLEGACYCLONE_API UDA_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	/** 이동 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	UInputAction* IA_Move;
+
+	/** 시점 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	UInputAction* IA_Look;
+
+	/** 점프 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	UInputAction* IA_Jump;
+
+	/** Ability 입력 (Tag 기반) */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	TMap<FGameplayTag, UInputAction*> AbilityInputActions;
+};
