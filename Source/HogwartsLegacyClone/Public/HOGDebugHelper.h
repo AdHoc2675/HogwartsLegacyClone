ed2e92d64df493c41f97a00e46976d7c4675dbb3
[Doheechoi] <rndtn47@gmail.com>
2026-03-04 16:39:44 +0900

    feat(GAS): GA_Base GameplayAbility 베이스 클래스 구현
    - GAS Ability 공통 베이스 클래스 UGA_Base 추가
    - Ability 기본 정책 설정 (InstancedPerActor, LocalPredicted)
    - Ability ActorInfo 접근 헬퍼 구현
      - GetHOGASC
      - GetPlayerController
      - GetPawn
      - GetCharacter
      - GetPlayerState
    - Ability Lifecycle 함수 추가
      - ActivateAbility
      - EndAbility
    - Ability 로그 함수(LogAbility) 구현
    - 프로젝트 공용 디버그 시스템(Core/HOG_Debug) 사용하도록 구성
    
    close #6

--- Source/HogwartsLegacyClone/Public/HOGDebugHelper.h
+++ Source/HogwartsLegacyClone/Public/HOGDebugHelper.h
@@ -4,24 +4,34 @@
 
 namespace Debug
 {
-    static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 InKey = -1)
+    inline void Print(
+        const FString& Msg,
+        const FColor& Color = FColor::MakeRandomColor(),
+        int32 InKey = -1,
+        float Duration = 7.f)
     {
         if (GEngine)
         {
-            GEngine->AddOnScreenDebugMessage(InKey, 7.0f, Color, Msg);
-
-            UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+            GEngine->AddOnScreenDebugMessage(InKey, Duration, Color, Msg);
         }
+
+        UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
     }
 
-    static void Print(const FString& FloatTitle, float FloatValueToPrint, int32 InKey = -1, const FColor& Color = FColor::MakeRandomColor())
+    inline void PrintFloat(
+        const FString& Title,
+        float Value,
+        int32 InKey = -1,
+        const FColor& Color = FColor::MakeRandomColor(),
+        float Duration = 7.f)
     {
+        const FString Msg = Title + TEXT(": ") + FString::SanitizeFloat(Value);
+
         if (GEngine)
         {
-            const FString FinalMsg = FloatTitle + TEXT(": ") + FString::SanitizeFloat(FloatValueToPrint);
-            GEngine->AddOnScreenDebugMessage(InKey, 7.0f, Color, FinalMsg);
-
-            UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMsg);
+            GEngine->AddOnScreenDebugMessage(InKey, Duration, Color, Msg);
         }
+
+        UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
     }
 }
