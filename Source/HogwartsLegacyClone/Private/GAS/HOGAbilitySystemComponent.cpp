53124ca5dbb329ad9fa2b70f17fcb0c5e0eaf371
[Doheechoi] <rndtn47@gmail.com>
2026-03-04 15:49:18 +0900

    feat: GAS 기초 구조 구축 (ASC / AttributeSet)
    - UHOGAbilitySystemComponent 생성
    - UHOGAttributeSet 생성
    - Attribute 구조 확정 (HP, MaxHP, Attack)
    
    close #5

--- Source/HogwartsLegacyClone/Private/GAS/HOGAbilitySystemComponent.cpp
+++ Source/HogwartsLegacyClone/Private/GAS/HOGAbilitySystemComponent.cpp
@@ -2,4 +2,97 @@
 
 
 #include "GAS/HOGAbilitySystemComponent.h"
+#include "Abilities/GameplayAbility.h"
 
+UHOGAbilitySystemComponent::UHOGAbilitySystemComponent()
+{
+}
+
+void UHOGAbilitySystemComponent::AbilityInputTagPressed(const FGameplayTag& InputTag)
+{
+	if (!InputTag.IsValid())
+	{
+		return;
+	}
+
+	TArray<FGameplayAbilitySpecHandle> Handles;
+	GetAbilitySpecHandlesByInputTag(InputTag, Handles);
+
+	for (const FGameplayAbilitySpecHandle& Handle : Handles)
+	{
+		if (!Handle.IsValid())
+		{
+			continue;
+		}
+
+		InputPressedSpecHandles.AddUnique(Handle);
+		InputHeldSpecHandles.AddUnique(Handle);
+	}
+}
+
+void UHOGAbilitySystemComponent::AbilityInputTagReleased(const FGameplayTag& InputTag)
+{
+	if (!InputTag.IsValid())
+	{
+		return;
+	}
+
+	TArray<FGameplayAbilitySpecHandle> Handles;
+	GetAbilitySpecHandlesByInputTag(InputTag, Handles);
+
+	for (const FGameplayAbilitySpecHandle& Handle : Handles)
+	{
+		if (!Handle.IsValid())
+		{
+			continue;
+		}
+
+		InputReleasedSpecHandles.AddUnique(Handle);
+		InputHeldSpecHandles.Remove(Handle);
+	}
+}
+
+void UHOGAbilitySystemComponent::ProcessAbilityInput(float DeltaTime, bool bGamePaused)
+{
+	for (const FGameplayAbilitySpecHandle& Handle : InputPressedSpecHandles)
+	{
+		FGameplayAbilitySpec* Spec = FindAbilitySpecFromHandle(Handle);
+		if (!Spec)
+		{
+			continue;
+		}
+
+		Spec->InputPressed = true;
+		TryActivateAbility(Handle);
+	}
+
+	for (const FGameplayAbilitySpecHandle& Handle : InputReleasedSpecHandles)
+	{
+		FGameplayAbilitySpec* Spec = FindAbilitySpecFromHandle(Handle);
+		if (!Spec)
+		{
+			continue;
+		}
+
+		Spec->InputPressed = false;
+		AbilitySpecInputReleased(*Spec);
+	}
+
+	InputPressedSpecHandles.Reset();
+	InputReleasedSpecHandles.Reset();
+}
+
+void UHOGAbilitySystemComponent::ClearAbilityInput()
+{
+	InputPressedSpecHandles.Reset();
+	InputReleasedSpecHandles.Reset();
+	InputHeldSpecHandles.Reset();
+}
+
+void UHOGAbilitySystemComponent::GetAbilitySpecHandlesByInputTag(const FGameplayTag& InputTag,
+	TArray<FGameplayAbilitySpecHandle>& OutHandles)
+{
+	InputPressedSpecHandles.Reset();
+	InputReleasedSpecHandles.Reset();
+	InputHeldSpecHandles.Reset();
+}
--- Source/HogwartsLegacyClone/Public/GAS/HOGAbilitySystemComponent.h
+++ Source/HogwartsLegacyClone/Public/GAS/HOGAbilitySystemComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayTagContainer.h"
 #include "HOGAbilitySystemComponent.generated.h"
 
 /**
@@ -13,5 +14,31 @@ UCLASS()
 class HOGWARTSLEGACYCLONE_API UHOGAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
+	
+public:
+
+	UHOGAbilitySystemComponent();
+
+	void AbilityInputTagPressed(const FGameplayTag& InputTag);
+	void AbilityInputTagReleased(const FGameplayTag& InputTag);
+
+	void ProcessAbilityInput(float DeltaTime, bool bGamePaused);
+
+	void ClearAbilityInput();
+
+protected:
+
+	void GetAbilitySpecHandlesByInputTag(const FGameplayTag& InputTag, TArray<FGameplayAbilitySpecHandle>& OutHandles);
+
+private:
+
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> InputPressedSpecHandles;
+
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> InputReleasedSpecHandles;
+
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> InputHeldSpecHandles;
 	
 };
