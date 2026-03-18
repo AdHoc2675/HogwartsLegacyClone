823f73b7ae5dbf4089b7bb067b6b8c5f22e510e1
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2026-03-18 16:23:22 +0900

    input 포함 lumos 초기 구현

--- Source/HogwartsLegacyClone/Public/Core/HOG_GameplayTags.cpp
+++ Source/HogwartsLegacyClone/Public/Core/HOG_GameplayTags.cpp
@@ -20,6 +20,7 @@ namespace HOGGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Input_Skill2, "Input.Skill2")
 	UE_DEFINE_GAMEPLAY_TAG(Input_Skill3, "Input.Skill3")
 	UE_DEFINE_GAMEPLAY_TAG(Input_Skill4, "Input.Skill4")
+	UE_DEFINE_GAMEPLAY_TAG(Input_Skill5, "Input.Skill5")
 
 	//State
 	UE_DEFINE_GAMEPLAY_TAG(State_Dead, "State.Dead")
